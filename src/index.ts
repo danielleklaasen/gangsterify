8cba254107ef0da9f0430a1ea1c9b16ebf46b571
Danielle Klaasen <danielleklaasen@gmail.com>
2020-02-26 13:35:38 +0000

    Update index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 'use strict'
 const ud = require('urban-dictionary')
 
-export function toSlang(input: string): string {
+export function getGangsterText(input: string): string {
     return ud.random().then((result: { word: string; definition: string; example: string; }) => {
         return result.example
     }).catch((error: { message: string; }) => {
